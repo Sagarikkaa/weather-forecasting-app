133e79764f4f520304766bb8d642d4b87bee70fe
Sagarikkaa <sahoo.sagarika2006@gmail.com>
2026-04-16 13:47:37 +0530

    Added all project files

---
+++ weatherproject/weatherapp/weatherapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.contrib import messages
+import requests
+import datetime
+def home(request):
+    if 'city' in request.POST:
+         city = request.POST['city']
+    else:
+         city = 'goa'
+PARAMS = {'units': 'metric'}
+API_KEY =  ''
+SEARCH_ENGINE_ID = ''
+query = city 
+searchType = 'image'
+query = city 
+page = 1
+start = (page - 1) * 10 + 1
+searchType = 'image'
+start = (page - 1) * 10 + 1
+city_url = fhttps://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge
+
+data = requests.get(city_url).json()
+search_items = data.get("items")
+ image_url = search_items[1]['link']
+try:
+          data = requests.get(url, params=PARAMS).json()
+          description = data['weather'][0]['description']
+icon = data['weather'][0]['icon']
+temp = data['main']['temp']
+day = datetime.date.today()
+
+
+return render(requests.request, 'weatherapp/index.html', {'description': description, 'icon': icon, 'temp': temp, 'day': day, 'city': city, 'exception_occurred': False, 'image_url': image_url})
+    except KeyError:
+exception_occurred = True
+          messages.error(request, 'Entered data is not available to API')
+day = datetime.date.today()
+return render(request, 'weatherapp/index.html', {'description': 'clear sky', 'icon': '01d', 'temp': 25, 'day': day, 'city': 'indore', 'exception_occurred': exception_occurred})
+
